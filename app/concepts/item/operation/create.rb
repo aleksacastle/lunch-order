fb6ab044fdfc708abde089dfc2b67db86fcc9057
aleksacastle <aaklochko@gmail.com>
2017-09-15 02:25:34 +0300

    add method to add item to menu

--- app/concepts/item/operation/create.rb
+++ app/concepts/item/operation/create.rb
@@ -1,7 +1,16 @@
 class Item::Create < Trailblazer::Operation
+  step :add_to_menu
   step Policy::Pundit(ItemPolicy, :create?)
   step Model(Item, :new)
   step Contract::Build(constant: Item::Contract::Create)
   step Contract::Validate()
   step Contract::Persist()
+
+  private
+
+  def add_to_menu(operation, params:, **)
+     model = Menu.find_by(id: params[:menu_id])
+     operation['model'] = model
+     operation['menu_id'] = model && model.id
+  end
 end
