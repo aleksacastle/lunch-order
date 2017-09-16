420f91163ea21e8c1a1c0f38eb45635240400e61
aleksacastle <aaklochko@gmail.com>
2017-09-16 22:32:47 +0300

    added def create to menu controller

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,15 @@
+class MenusController < ApplicationController
+
+  def new
+    run Menu::Create
+  end
+
+  def create
+    run Menu::Create do |result|
+      flash[:notice] = "Created menu for today"
+      return redirect_to menu_path(result["model"].id)
+    end
+
+    render :new
+  end
+end
