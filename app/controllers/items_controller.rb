6d42470a52341124fe9d46df51de2cda977a8d4e
aleksacastle <aaklochko@gmail.com>
2017-09-18 02:02:00 +0300

    item controller new||create||index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,21 @@
 class ItemsController < ApplicationController
 
   def new
-    form Item::Create
+    run Item::Create::Present
+    render cell(Item::Cell::CreateForm, @form)
   end
 
   def create
     run Item::Create do |op|
       flash[:notice] = "Add: [#{op.model.name}] to \"#{op.model.category}\""
-
-      return redirect_to lunch_path(op.model)
+      return redirect_to menu_path(op.model)
     end
 
-    render action: :new
+    render cell(Item::Cell::CreateForm, @form)
+  end
+
+  def index
+    run Item::Index
+    render cell(Item::Cell::Index, result["model"])
   end
 end
