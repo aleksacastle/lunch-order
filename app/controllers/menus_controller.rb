17cec427aed817c6ab2a1056ea00f9e89c87f04d
aleksacastle <aaklochko@gmail.com>
2017-09-17 20:20:46 +0300

    menu controller new||create||index||show

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,19 +1,26 @@
 class MenusController < ApplicationController
 
   def show
-    render cell(Menu::Cell::Show, @form)
+    run Menu::Show
+    render cell(Menu::Cell::Show, result["model"])
   end
 
   def new
-    run Menu::Create
+    run Menu::Create::Present
+    render cell(Menu::Cell::CreateForm, @form)
   end
 
   def create
     run Menu::Create do |result|
       flash[:notice] = "Created menu for today"
-      return redirect_to menu_path(result["model"].id)
+      return redirect_to menus_path
     end
 
-    render :new
+    render cell(Menu::Cell::CreateForm, @form)
+  end
+
+  def index
+    run Menu::Index
+    render cell(Menu::Cell::Index, result["model"])
   end
 end
