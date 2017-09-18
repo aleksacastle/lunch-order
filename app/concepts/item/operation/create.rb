100381b97c4e3f2658123b2eb1f2084dd5d6ce9a
aleksacastle <aaklochko@gmail.com>
2017-09-19 00:00:49 +0300

    create item functionality

--- app/concepts/item/operation/create.rb
+++ app/concepts/item/operation/create.rb
@@ -1,20 +1,21 @@
 class Item::Create < Trailblazer::Operation
-  # step :add_to_menu
   class Present < Trailblazer::Operation
-    # step Policy::Pundit(ItemPolicy, :create?)
+
     step Model(Item, :new)
+    step :add_to_menu!
+    # step Policy::Pundit(ItemPolicy, :create?)
     step Contract::Build(constant: Item::Contract::Create)
+
+    private
+
+    def add_to_menu!(options, model:, params:, **)
+      # raise model.inspect
+        model.menu = Menu.find_by(id: params[:menu_id])
+      #  model.menu_id = Menu.find_by(id: params[:menu_id])
+    end
   end
-  
+
   step Nested(Present)
   step Contract::Validate()
   step Contract::Persist()
-
-  private
-
-  def add_to_menu(operation, params:, **)
-     model = Menu.find_by(id: params[:menu_id])
-     operation['model'] = model
-     operation['menu_id'] = model && model.id
-  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,23 @@ class ItemsController < ApplicationController
   def create
     run Item::Create do |op|
       flash[:notice] = "Add: [#{op.model.name}] to \"#{op.model.category}\""
-      return redirect_to menu_path(op.model)
-    end
+      return redirect_to menu_item_path(op['model'].id)
 
-    render cell(Item::Cell::CreateForm, @form)
+    end
+    render cell( Item::Cell::CreateForm, @form )
+
   end
 
   def index
     run Item::Index
     render cell(Item::Cell::Index, result["model"])
+  end
+
+  private
+
+  def menu_op(item_id)
+    run Menu::Create::Present,
+        { item_id: item_id }
+        # 'current_user' => tyrant.current_user
   end
 end
