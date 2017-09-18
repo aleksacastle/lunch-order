fdf413f0b2c777f128483fd36dee5ceb71df296c
aleksacastle <aaklochko@gmail.com>
2017-09-18 23:59:35 +0300

    add menu_id column to item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918112237) do
+ActiveRecord::Schema.define(version: 20170918202550) do
 
   create_table "items", force: :cascade do |t|
     t.integer "category"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170918112237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.index ["menu_id"], name: "index_items_on_menu_id"
   end
 
   create_table "lunches", force: :cascade do |t|
