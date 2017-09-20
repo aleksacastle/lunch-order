45b140095527e86fbd871738b0c7468fdd9d842d
aleksacastle <aaklochko@gmail.com>
2017-09-20 20:48:43 +0300

    item show concept||controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,28 +3,25 @@ class ItemsController < ApplicationController
   def new
     run Item::Create::Present
     render cell(Item::Cell::CreateForm, @form)
+
   end
 
   def create
-    run Item::Create do |op|
-      flash[:notice] = "Add: [#{op.model.name}] to \"#{op.model.category}\""
-      return redirect_to menu_item_path(op['model'].id)
-
+    run Item::Create do |result|
+      flash[:notice] = "Added"
+      return redirect_to menu_item_path(result['model'].id)
     end
     render cell( Item::Cell::CreateForm, @form )
 
   end
 
+  def show
+    run Item::Show
+    render cell(Item::Cell::Show, result["model"])
+  end
+
   def index
     run Item::Index
     render cell(Item::Cell::Index, result["model"])
-  end
-
-  private
-
-  def menu_op(item_id)
-    run Menu::Create::Present,
-        { item_id: item_id }
-        # 'current_user' => tyrant.current_user
   end
 end
