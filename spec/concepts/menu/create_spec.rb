95c8af58342f9412a64f417f26d29b3561c94077
aleksacastle <aaklochko@gmail.com>
2017-09-24 22:51:32 +0300

    test of menu_create concept

--- spec/concepts/menu/create_spec.rb
+++ spec/concepts/menu/create_spec.rb
@@ -1,14 +1,19 @@
 require "rails_helper"
-require_relative "../../../app/concepts/menu/operation/create"
 
 RSpec.describe Menu::Create do
-it "runs successfully" do
-  params = { weekday: "monday" }
+  it "prohibits empty params" do
+    result = Menu::Create.({})
 
-  result = Menu::Create.( params )
+    expect(result).to be_failure
+  end
 
-  # expect(result).to be_success
-  # expect(result["model"]).to be_persisted
-  expect(result["model"].weekday).to eq "monday"
-end
+  it "runs successfully" do
+    params = { weekday: "monday" }
+
+    result = Menu::Create.( menu: params )
+
+    expect(result).to be_success
+    expect(result["model"]).to be_persisted
+    expect(result["model"].weekday).to eq "monday"
+  end
 end
